9fb696b2466bd8c49f5c24ff9ac2706a7338e4d8
DanielMikolajczyk <56119744+DanielMikolajczyk@users.noreply.github.com>
2022-02-16 10:11:58 +0100

    Frontend assets

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,11 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import './styles/global.scss';
+
+import $ from 'jquery';
+
+$(document).ready(function() {
+    
+});
